02292340541f34d965917f3f61c6857790850ec4
javaer.ljw <javaer.ljw@alibaba-inc.com>
2021-05-01 15:32:22 +0800

    0.0.3

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,9 +1,7 @@
 # Change Log
 
-All notable changes to the "git-url-copy" extension will be documented in this file.
-
-Check [Keep a Changelog](http://keepachangelog.com/) for recommendations on how to structure this file.
-
-## [Unreleased]
-
-- Initial release
+0.0.3
+get repository base from
+  1. package.json reposition.url
+  2. git remote origin
+  3. git repository customizable, by configuration "gitUrlCopy.repositoryBase"
--- src/extension.ts
+++ src/extension.ts
@@ -4,24 +4,30 @@ import * as fs from 'fs';
 import * as path from 'path';
 import * as childProcess from 'child_process';
 
-//Write to output.
 export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(vscode.commands.registerTextEditorCommand('extension.copyGitPath', (textEditor, edit) => {
 		const line = textEditor.selection.start.line + 1;
 		const file = textEditor.document.uri.path;
 		const fileRelativePath = vscode.workspace.asRelativePath(file);
 		const cwd = path.dirname(file);
-		
-		// https://code.aone.alibaba-inc.com/it-service/alilang/blob/master/src/i18n/index.ts#L6
-		const webUrl = `https://code.aone.alibaba-inc.com/${getRepository(cwd)}/blob/${getCurrentBranch(cwd)}/${fileRelativePath}#L${line}`;
-		clipboard.write('code copy success' + webUrl);
-		vscode.window.showInformationMessage(webUrl);
+		try {
+			let repositoryBase = getRepositoryBaseFromPackageJson(file) || getRepository(cwd);
+			if (!repositoryBase) throw new Error('cant\' find git repository');
+			repositoryBase = gitPath2Http(repositoryBase);
+			
+			// https://code.aone.alibaba-inc.com/it-service/alilang/blob/master/src/i18n/index.ts#L6
+			const webUrl = `${repositoryBase}/blob/${getCurrentBranch(cwd)}/${fileRelativePath}#L${line}`;
+			clipboard.write(webUrl);
+			vscode.window.showInformationMessage('code copy success ' + webUrl);
+		} catch (e) {
+			vscode.window.showWarningMessage('copy failed ' + e.message);
+		}
 	}));
 }
 
 
 function getCurrentBranch(cwd?: string) {
-	return childProcess.execSync('git branch --show-current', {encoding: 'utf8', cwd});
+	return childProcess.execSync('git branch --show-current', {encoding: 'utf8', cwd}).trim();
 }
 
 function getRepository(cwd?: string) {
@@ -30,4 +36,25 @@ function getRepository(cwd?: string) {
 		// git@gitlab.alibaba-inc.com:platform/alinw-live.git
 		return remoteUrl.match(':(.*)\.git')![1];
 	}
+}
+
+function getRepositoryBaseFromPackageJson(file: string): string {
+	const folder2Scan = fs.statSync(file).isFile() ? path.dirname(file) : file;
+	const jsonPath = path.resolve(folder2Scan, 'package.json');
+  if (fs.existsSync(jsonPath)) {
+    const repo = require(jsonPath).repository;
+    return repo ? repo.url : '';
+  } else {
+    const nextScan = path.dirname(folder2Scan);
+    if (nextScan === folder2Scan) return '';
+    return getRepositoryBaseFromPackageJson(nextScan)
+  }
+}
+
+function gitPath2Http(g: string) {
+	const match = g.match(/git@(.*):(.*)\/(.*)\.git/);
+	if (!match) return g;
+	const repositoryBase = vscode.workspace.getConfiguration().get<Array<Array<string>>>('gitUrlCopy.repositoryBase') || [];
+	const customRepository = repositoryBase.find(pair => pair[0] === match[1])?.[1];
+	return `https://${customRepository || match[1]}/${match[2]}/${match[3]}`;
 }
